aeb565ee77a1f146ca179213569c70cb867b6d73
Andres Perez <aperez@salesforce.com>
2021-06-03 08:11:06 -0400

    Updated Image

--- deploy/main/default/lwc/homePage/homePage.js
+++ deploy/main/default/lwc/homePage/homePage.js
@@ -1,5 +1,5 @@
 import { LightningElement } from "lwc";
-import OOE_RING from "@salesforce/contentAssetUrl/ring";
+import OOE_RING from "@salesforce/contentAssetUrl/OOE_SUM21";
 
 export default class HomePage extends LightningElement {
 	// Expose the asset file URL for use in the template
