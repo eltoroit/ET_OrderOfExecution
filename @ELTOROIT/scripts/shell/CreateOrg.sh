96fcf7df86ada3fb5eee2be265f8cfdd37529269
Andres Perez <aperez@salesforce.com>
2022-08-31 07:09:46 -0400

    Mac scripts to create org

--- @ELTOROIT/scripts/shell/CreateOrg.sh
+++ @ELTOROIT/scripts/shell/CreateOrg.sh
@@ -13,15 +13,20 @@
 	
 	# Install required packages before pushing
 	# Sample: PACKAGES=("04tB0000000P1yA" "04tB0000000P1yB" "04tB0000000P1yC")
-	# PACKAGES=("04tB0000000P1yA")
+	PACKAGES=
 
-	# Permission Set name
-	PERM_SET=OOE
+	# Permission Set names
+	# Sample: PACKAGES=("PS_01" "PS_02" "PS_03")
+	PERM_SETS=("OOE")
+
+	# Prepare scratch org for deployment. Some metadata needs to be deployed first.
+	# Sample: PREPARE_ORG=("@ELTOROIT/metadata/API/")
+	# PREPARE_ORG=("@ELTOROIT/metadata/API/")
 	
-	# Path to Apex code to execute anonymously
-	# Sample: $DIR/AnonymousApex.txt
-	EXEC_ANON_APEX_BEFORE_PUSH=
-	EXEC_ANON_APEX_AFTER_PUSH=$DIR/../apex/SetUserRecord.apex
+	# Path to Apex code to execute anonymously, multiple files can be executed at each step, if multiple Apex transactions are needed
+	# Sample: ("$DIR/File1.txt" "$DIR/File2.txt")
+	EXEC_ANON_APEX_BEFORE_PUSH= 
+	EXEC_ANON_APEX_AFTER_PUSH=("$DIR/../apex/SetUserRecord.apex" )
 	EXEC_ANON_APEX_AFTER_DATA=
 	
 	# Is there any additional manual configuration required BEFORE pushing metadata?
@@ -32,7 +37,24 @@
 	# Sample: /lightning/setup/SalesforceMobileAppQuickStart/home
 	PATH2SETUP_METADATA_AFTER=
 
+	# Do you have a community to publish?
+	# Sample: PUBLISH_COMMUNITY_NAME="Community Name"
+	PUBLISH_COMMUNITY_NAME=
+
+	# What is the sandbox to deply to?
+	# Sample: DEPLOY_TO_SANDBOX="Sandbox sfdx alias"
+	DEPLOY_TO_SANDBOX=
+
+	# Sample: DEPLOY_TO_SANDBOX_FOLDER="./force-apps/deploy"
+	DEPLOY_TO_SANDBOX_FOLDER=
+
+	# If using ETCopyData, where is the data?
+	ETCOPYDATA_FOLDER="./@ELTOROIT/data"
+
 # --- Batch boolean variables
+
+	# Do you want to use ETCopyData to import data?
+	IMPORT_DATA=true
 
 	# Quit on errors?
 	QUIT_ON_ERRORS=true
@@ -56,10 +78,7 @@
 	PERFORM_DEPLOY=true
 
 	# Deploy Admin standard profile (helps set the visible apprlications, for example)
-	DEPLOY_ADMIN=true
-
-	# Do you want to use ETCopyData to import data?
-	IMPORT_DATA=true
+	ADMIN_PROFILE=force-apps/doNotDeploy/main/default/profiles/Admin.profile-meta.xml
 
 	# Do you want to run APEX tests in this new org before starting?
 	RUN_APEX_TESTS=true
