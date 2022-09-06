e9d798755ea1f65eee9c5a27aa468f109f79a1ad
Andres Perez <aperez@salesforce.com>
2022-09-06 11:28:32 -0400

    Home tab updated with hew bracelet

--- deploy/main/default/lwc/homePage/homePage.js
+++ deploy/main/default/lwc/homePage/homePage.js
@@ -1,5 +1,5 @@
 import { LightningElement } from "lwc";
-import OOE_RING from "@salesforce/contentAssetUrl/OOE_SUM21";
+import OOE_RING from "@salesforce/contentAssetUrl/OOE_WIN23";
 
 export default class HomePage extends LightningElement {
 	// Expose the asset file URL for use in the template
